d482c59c5c9cbd8cc02543404c573b8e6acdd04e
Amol Mundhe <97632239+Ammundhe@users.noreply.github.com>
2023-08-13 13:46:24 +0000

    Update 3 files

--- src/Component/Calender/Calender.jsx
+++ src/Component/Calender/Calender.jsx
@@ -1,57 +1,105 @@
-import React from "react";
+import { useState } from "react";
 import "./Calender.css";
-import { CaretLeft, CaretRight } from "phosphor-react";
+import { CaretLeft, CaretRight, ArrowLeft, ArrowRight } from "phosphor-react";
 
 const Calender = () => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const [year, setYear] = useState(2023); // Initial year
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const [monthIndex, setMonthIndex] = useState(1); // Initial month index (February)
+  const firstDayOfMonth = new Date(year, monthIndex, 1).getDay(); // Get the day of the week the month starts on
+  console.log(firstDayOfMonth);
+  const startingDay = (firstDayOfMonth + 6) % 7; // Adjust the starting day to account for Sunday being the start of the week
+
+  const handlePrevMonth = () => {
+    if (monthIndex === 0) {
+      setYear(year - 1);
+      setMonthIndex(11); // Set to December
+    } else {
+      setMonthIndex(monthIndex - 1);
+    }
+  };
+
+  const handleNextMonth = () => {
+    if (monthIndex === 11) {
+      setYear(year + 1);
+      setMonthIndex(0); // Set to January
+    } else {
+      setMonthIndex(monthIndex + 1);
+    }
+  };
+
+  const handlePrevYear = () => {
+    setYear(year - 1);
+  };
+
+  const handleNextYear = () => {
+    setYear(year + 1);
+  };
+
+  const daysInMonth = new Date(year, monthIndex + 1, 0).getDate(); // Get days in current month
+
+  const daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1); // Create an array of days
   return (
     <section className="calender_container">
       <div className="calender_header">
-        <h3>February</h3>
+        <div className="calender_month">
+          <ArrowLeft
+            className="calender_icon"
+            weight="bold"
+            onClick={handlePrevMonth}
+          />
+          <h3>{months[monthIndex]}</h3>
+          <ArrowRight
+            className="calender_icon"
+            weight="bold"
+            onClick={handleNextMonth}
+          />
+        </div>
         <div className="calender_year">
-          <CaretLeft className="calender_icon" weight="bold" />
-          <h3>2023</h3>
-          <CaretRight className="calender_icon" weight="bold" />
+          <CaretLeft
+            className="calender_icon"
+            weight="bold"
+            onClick={handlePrevYear}
+          />
+          <h3>{year}</h3>
+          <CaretRight
+            className="calender_icon"
+            weight="bold"
+            onClick={handleNextYear}
+          />
         </div>
       </div>
       <div className="calender_monthly_container">
         <div className="calender_days_of_week space_between">
-          <h4 className="calender_item">Sun</h4>
-          <h4 className="calender_item">Mon</h4>
-          <h4 className="calender_item">Tue</h4>
-          <h4 className="calender_item">Wed</h4>
-          <h4 className="calender_item">Thu</h4>
-          <h4 className="calender_item">Fri</h4>
-          <h4 className="calender_item">Sat</h4>
+          {daysOfWeek.map((day) => (
+            <h4 key={day} className="calender_item">
+              {day}
+            </h4>
+          ))}
         </div>
         <div className="calender_days_of_month space_between">
-          <h4 className="calender_item">1</h4>
-          <h4 className="calender_item">2</h4>
-          <h4 className="calender_item">3</h4>
-          <h4 className="calender_item">4</h4>
-          <h4 className="calender_item">5</h4>
-          <h4 className="calender_item">6</h4>
-          <h4 className="calender_item">7</h4>
-          <h4 className="calender_item">8</h4>
-          <h4 className="calender_item">9</h4>
-          <h4 className="calender_item">10</h4>
-          <h4 className="calender_item">11</h4>
-          <h4 className="calender_item">12</h4>
-          <h4 className="calender_item">13</h4>
-          <h4 className="calender_item">14</h4>
-          <h4 className="calender_item">15</h4>
-          <h4 className="calender_item">16</h4>
-          <h4 className="calender_item">17</h4>
-          <h4 className="calender_item">18</h4>
-          <h4 className="calender_item">19</h4>
-          <h4 className="calender_item">20</h4>
-          <h4 className="calender_item">21</h4>
-          <h4 className="calender_item">22</h4>
-          <h4 className="calender_item">23</h4>
-          <h4 className="calender_item">24</h4>
-          <h4 className="calender_item">25</h4>
-          <h4 className="calender_item">26</h4>
-          <h4 className="calender_item">27</h4>
-          <h4 className="calender_item">28</h4>
+          {Array(startingDay)
+            .fill(null)
+            .map((_, index) => (
+              <div key={`empty-${index}`} className="calender_item"></div>
+            ))}
+          {daysArray.map((day) => (
+            <p className="calender_item">{day}</p>
+          ))}
         </div>
       </div>
     </section>
